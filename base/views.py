20a7331c1c3b12c9ff1f0f3007b0beb7cc7614d1
Jared Ruiz <jared707@gmail.com>
2022-12-16 13:21:39 -0600

    Added task.html, added routing to id specific items. Changed default naming scheme in views.py

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 #returns template with query set of data
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
 #simple http response import
@@ -9,7 +10,16 @@ from .models import Task
 
 # Create your views here.
 # This is connected to urls.py and can accessed through there as well
+# This looks for _list
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
+
+# This looks for _detail
+# template name and context object name allow us to change the default naming scheme for html
+# and object name
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
     
